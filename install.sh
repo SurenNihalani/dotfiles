ae3d9b0818040acd59951b54b852818818c0217b
Suren Nihalani <suren@quora.com>
2017-06-01 19:43:48 +0000

    fzf: install

--- install.sh
+++ install.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 set -x
+
+# on an average, setup should be reproducble
+rm -f ~/.zshrc
 cp zshrc ~/.zshrc
+
+# fzf is pretty key
+rm -rf ~/.fzf
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
